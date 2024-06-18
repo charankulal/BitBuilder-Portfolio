1a547e737cf76665aacbe819b1f2e69da5fb89cb
Charan <charankulal0241@gmail.com>
2024-06-18 18:47:22 +0530

    Completed grid section/about section

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -5,17 +5,21 @@ import { gridItems } from '@/data'
 const Grid = () => {
   return (
     <section id='about'>
-        <BentoGrid>
+        <BentoGrid className="w-full py-20">
             {gridItems.map((item,i)=>(
                 <BentoGridItem
                 id={item.id}
                 key={item.id}
                 title={item.title}
                 description={item.description}
+                className={item.className}
+                img={item.img}
+                imgClassName={item.imgClassName}
+                titleClassName={item.titleClassName}
+                spareImg={item.spareImg}
                 />
             ))}
         </BentoGrid>
-      
     </section>
   )
 }
